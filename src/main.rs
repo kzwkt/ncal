f0de8a45c8420324fcb2466526204121e868627d
razzat008 <93041325+razzat008@users.noreply.github.com>
2026-06-16 13:18:07 +0545

    add: main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use ncal::{app, cli::Cli, config};
 
 fn main() {
     let cli = Cli::parse();
-    if let Some(config) = config::Config::from_cli(&cli).ok() {
-        if let Err(e) = config.validate() {
-            eprintln!("Error: {e}");
-        } else {
-            app::run(config).unwrap_or_else(|e| eprintln!("Error: {e}"));
+    match config::Config::from_cli(&cli) {
+        Ok(config) => {
+            if let Err(e) = config.validate() {
+                eprintln!("Error: {e}");
+            } else if let Err(e) = app::run(config) {
+                eprintln!("Error: {e}");
+            }
         }
+        Err(e) => eprintln!("Error: {e}"),
     }
 }
